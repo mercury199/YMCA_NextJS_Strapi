c8f4d982957b3ea11a0d76b916ceba5a8b9aaca2
Alexander Bisant <mercury199@gmail.com>
2024-03-06 11:00:20 -0700

    Update server.js
    Added in Strapi App Keys

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('41b41cec280e38e0e14d791d3a416982','dec7cfdcf1cbd09ed7587559fef47dd2'),
   },
   url: env('STRAPI_URL'),
 });
